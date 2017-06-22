535341c30071091f760fd17b2e70f4ebd8f8e8d8
DESKTOP-G1SPGPH\김제혁 <os2dr>
2017-06-22 21:39:13 +0900

    Test

--- GraphicsFinalwork/Sources/dxutil.h
+++ GraphicsFinalwork/Sources/dxutil.h
@@ -3,7 +3,7 @@
 
 
 //-----------------------------------------------------------------------------
-// Miscellaneous helper functions
+// Miscellaneous helper functions0
 //-----------------------------------------------------------------------------
 #define SAFE_DELETE(p)       { if(p) { delete (p);     (p)=NULL; } }
 #define SAFE_DELETE_ARRAY(p) { if(p) { delete[] (p);   (p)=NULL; } }

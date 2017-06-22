0e3bf2481ab02e60e391b81f8bd3141026a11e2f
DESKTOP-G1SPGPH\김제혁 <os2dr>
2017-06-22 21:23:17 +0900

    이전파일 삭제

---
+++ GraphicsFinalwork/Sources/fps.h
@@ -0,0 +1,23 @@
+#ifndef __fpsH__
+#define __fpsH__
+
+#include "d3dfont.h"
+
+class FPSCounter
+{
+public:
+	FPSCounter(IDirect3DDevice9* device);
+	~FPSCounter();
+
+	bool render(D3DCOLOR color, float timeDelta);
+private:
+	IDirect3DDevice9* _device;
+	
+	CD3DFont* _font;
+	DWORD     _frameCnt;
+	float     _timeElapsed;
+	float     _fps;
+	char      _fpsString[9];
+	
+};
+#endif // __fpsH__
